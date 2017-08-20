c293717ff2aacbb43ec30b22573e70194bfa4c1d
Dylan Marsh <sushiboxdev@gmail.com>
2017-08-20 17:17:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+![AURA](https://hostr.co/file/970/YSADRZhRvW4A/aura-logo.png)
+
+![Owned By Acculevel](https://img.shields.io/badge/Owned%20By-Acculevel-red.svg) ![Powered By Marsh Multimedia](https://img.shields.io/badge/Powered%20By-Marsh%20Multimedia-blue.svg)
+
+> A U R A is an extremely simple error reporting service.
+
+  - So stupid simple that it only has a few methods
+  - Provides an overly opinionated wrapper for logging error with Loggly
+  - Shows pretty crashes in the console via Chalk
+
+### A U R A
+```
+ By: Dylan Marsh <dylan@acculevel.com>
+ Acculevel Inc - http://acculevel.com
+ Marsh Multimedia DBA Knoware - http://knoware.co
+ (C) 2017 Acculevel Inc. - All Rights Reserved
+```
---
+++ index.js
@@ -0,0 +1,39 @@
+const chalk = require('chalk');
+const winston = require('winston');
+const winlog = require('winston-loggly-bulk');
+
+const attach = (token, subdomain, json=true, tags=["Winston-NodeJS"]) => {
+    winston.add(winston.transports.Loggly, {token, subdomain, tags, json});
+};
+
+const error = err => {
+    winston.log('error', err);
+    throw new Error(err);
+};
+
+const log = msg => {
+    winston.log('info', msg);
+};
+
+const errorHandler = () => {
+    return (err, req, res, next) => {
+        if (process.env.NODE_ENV === 'development') {
+            console.log(chalk.bold.bgRed('\n-= A U R A  E R R O R  R E P O R T E R =-\n'));
+
+            if (req.route) {
+                console.log(chalk.bold.green(`ROUTE: ${req.route.path}  &&  METHOD: ${req.route.stack[0].method.toUpperCase()}\n`));
+            }
+
+            console.error(chalk.bold(err.stack) + '\n');
+        }
+
+        res.status(500).send(err.stack);
+    }
+};
+
+module.exports = {
+    attach,
+    error,
+    log,
+    errorHandler
+};
