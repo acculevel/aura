c61b9f4409658e2833e28179592154ecd749c777
Dylan Marsh <sushiboxdev@gmail.com>
2017-08-21 09:19:13 -0400

    Going back

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-const app = require('express')();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const chalk = require('chalk');
+const winston = require('winston');
+const winlog = require('winston-loggly-bulk');
 
-server.listen(8080);
-
-app.get('/', (req, res) => {
-    res.sendfile(__dirname + '/index.html');
-});
-
-io.on('connection', socket => {
-    socket.emit('news', {hello: 'world'});
-    socket.on('my other event', data => {
-        console.log(data);
+const attach = opts => {
+    winston.add(winston.transports.Loggly, {
+        token: opts.token,
+        subdomain: opts.subdomain,
+        tags: ["Winston-NodeJS"],
+        json: true
     });
-});
+};
+
+const error = err => {
+    winston.log('error', err);
+    throw new Error(err);
+};
+
+const log = msg => {
+    winston.log('info', msg);
+};
+
+const _handle404 = (req, res, next) => {
+    res.status(404).json({
+        errors: ['ResourceNotFound'],
+        message: 'The resource you are requesting does not exist or was moved.'
+    });
+};
+
+const _handle5xx = (err, req, res, next) => {
+    if (process.env.NODE_ENV === 'development') {
+        console.log(chalk.bold.bgRed('\n-= A U R A  E R R O R  R E P O R T E R =-\n'));
+
+        if (req.route) {
+            console.log(chalk.bold.green(`ROUTE: ${req.route.path}  &&  METHOD: ${req.route.stack[0].method.toUpperCase()}\n`));
+        }
+
+        console.error(chalk.bold(err.stack) + '\n');
+    }
+
+    res.status(500).send(err.stack);
+};
+
+const handler = app => {
+    [_handle404, _handle5xx].forEach(middleware => {
+        app.use(middleware);
+    });
+};
+
+module.exports = {
+    attach,
+    error,
+    log,
+    handler
+};
