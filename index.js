83dca709f3ffcb4a7c0cae5712eecbe864f8f1f9
Dylan Marsh <sushiboxdev@gmail.com>
2017-08-20 20:13:14 -0400

    Better handling of not found

--- index.js
+++ index.js
@@ -20,28 +20,26 @@ const log = msg => {
     winston.log('info', msg);
 };
 
-const notFoundHandler = () => {
-    return (req, res, next) => {
-        res.status(404).json({
-            errors: ['ResourceNotFound'],
-            message: 'The resource you are requesting does not exist or was moved.'
-        });
-    }
-};
-
 const errorHandler = () => {
     return (err, req, res, next) => {
-        if (process.env.NODE_ENV === 'development') {
-            console.log(chalk.bold.bgRed('\n-= A U R A  E R R O R  R E P O R T E R =-\n'));
+        if (err instanceof NotFound) {
+            res.status(404).json({
+                errors: ['ResourceNotFound'],
+                message: 'The resource you are requesting does not exist or was moved.'
+            });
+        } else {
+            if (process.env.NODE_ENV === 'development') {
+                console.log(chalk.bold.bgRed('\n-= A U R A  E R R O R  R E P O R T E R =-\n'));
 
-            if (req.route) {
-                console.log(chalk.bold.green(`ROUTE: ${req.route.path}  &&  METHOD: ${req.route.stack[0].method.toUpperCase()}\n`));
+                if (req.route) {
+                    console.log(chalk.bold.green(`ROUTE: ${req.route.path}  &&  METHOD: ${req.route.stack[0].method.toUpperCase()}\n`));
+                }
+
+                console.error(chalk.bold(err.stack) + '\n');
             }
 
-            console.error(chalk.bold(err.stack) + '\n');
+            res.status(500).send(err.stack);
         }
-
-        res.status(500).send(err.stack);
     }
 };
 
@@ -49,6 +47,5 @@ module.exports = {
     attach,
     error,
     log,
-    notFoundHandler,
     errorHandler
 };
